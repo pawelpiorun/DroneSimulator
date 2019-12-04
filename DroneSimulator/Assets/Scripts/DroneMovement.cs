b6b350be1a39a4a22c22b852425bf367f4bcf188
Paweł Piorun <dev.pawelpiorun@gmail.com>
2019-12-04 22:35:54 +0100

    Add input axis, drone representation, ground representation and drone movement script

---
+++ DroneSimulator/Assets/Scripts/DroneMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour {
+
+    float speed = 5.0f;
+    float rotateStep = 1.0f;
+    Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        var throttle = Input.GetAxis("Throttle");
+        var yaw = Input.GetAxis("Yaw");
+        var pitch = Input.GetAxis("Pitch");
+        var roll = Input.GetAxis("Roll");
+
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        var verticalMovement = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(0, throttle * speed, 0);
+
+        rigidbody.AddTorque(pitch, yaw, roll, ForceMode.Acceleration);
+
+	}
+}
